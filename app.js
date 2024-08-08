db962169dc4fe6245be0fa60e44dc4bdb383b618
nwmason <[200~174368433+nwmason@users.noreply.github.com>
2024-08-08 12:19:49 -0400

    semi-functional numbers appending, font size adjustment

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ function calculate() {
 
 }
 
+function appendNumber(number) {
+    workingResults.innerHTML += number
+}
+
+function appendOperation() {
+
+}
+
+function errorMessage() {
+
+}
+
 const buttons = document.querySelectorAll(".calculator-button");
+const workingResults = document.querySelector(".results-screen-typing");
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", (event) => {
-        console.clear()
-        console.log(event.target.id);
-        console.log('clicked');
+
+        if (isNaN(event.target.id) === false) {
+            appendNumber(event.target.id);
+        } else {
+
+        }
     });
 }
