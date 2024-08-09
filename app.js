b86035a7cbedc15e964bfc58d95c4384612dd112
nwmason <[200~174368433+nwmason@users.noreply.github.com>
2024-08-09 13:44:47 -0400

    incredibly hacky workaround, i have no clue what i'm doing anymore

--- app.js
+++ app.js
@@ -7,12 +7,50 @@ function equaltsTo() {
             || workingResults.textContent.slice(-1) === ".") {
         errorMessage("ERROR: IMPROPER EQUATION");
     } else {
+        operationCount = 0
         calculate();
     }
 }
 
 function calculate() {
+    let splitEquation = workingResults.textContent.split(" ")
+    let addition = null
+    let subtraction = null
+    let multiplication = null
+    let division = null
+    let operationIndex = null
 
+    for (let i = 0; i < splitEquation.length; i++) {
+        if (!isNaN(parseFloat(splitEquation[i]))) {
+        } else {
+            operationIndex = i
+            }
+        }  
+
+    switch(splitEquation[operationIndex]) {
+        case '+':
+            console.log("addition");
+            result = parseFloat(splitEquation[operationIndex-1]) + parseFloat(splitEquation[operationIndex+1])
+            break;
+        case '-':
+            console.log("subtraction");
+            result = parseFloat(splitEquation[operationIndex-1]) - parseFloat(splitEquation[operationIndex+1])
+            break;
+        case 'x':
+            console.log("multiplication");
+            result = parseFloat(splitEquation[operationIndex-1]) * parseFloat(splitEquation[operationIndex+1])
+            break;
+        case '÷':
+            console.log("division");
+            result = parseFloat(splitEquation[operationIndex-1]) / parseFloat(splitEquation[operationIndex+1])
+            break;
+        case '%':
+            console.log(percent);
+            break;
+        default:
+            finalResults.textContent = workingResults.textContent
+    }
+    finalResults.textContent = result;
 }
 
 function errorMessage(error) {
@@ -27,6 +65,7 @@ function errorMessage(error) {
 function appendNumber(number) {
     if (workingResults.textContent.length > 35) {
         errorMessage("ERROR: SCREEN OVERFLOW");
+    } else if(percentCount === 1) {
     } else {
         workingResults.textContent += number;
     }
@@ -40,10 +79,13 @@ function appendOperation(operation) {
     } else if (currentEquation.length === 0
         || currentEquation.slice(-1) === ".") {
         return
+    } else if (operationCount === 1) {
+        return
     } else {
         workingResults.innerHTML += " " + operation + " ";
         decimalCount = 0
         percentCount = 0
+        operationCount++
     }
 }
 
@@ -96,11 +138,14 @@ function clearData(clearMethod) {
             decimalCount = 0;
         } else {
             workingResults.textContent = currentEquation.slice(0, -1);
+            finalResults.textContent = "";
         }
     } else {
-        workingResults.textContent = ""
+        workingResults.textContent = "";
+        finalResults.textContent = "";
         percentCount = 0;
         decimalCount = 0;
+        operationCount = 0;
     }
 }
 
@@ -109,12 +154,13 @@ const workingResults = document.querySelector(".results-screen-typing");
 const finalResults = document.querySelector(".results-screen-result");
 let decimalCount = 0
 let percentCount = 0
+let operationCount = 0
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", (event) => {
 
         if (isNaN(event.target.id) === false) {
-            appendNumber(event.target.id);
+            appendNumber(parseInt(event.target.id));
 
         } else if (event.target.id === "percent") {
             appendPercent();
